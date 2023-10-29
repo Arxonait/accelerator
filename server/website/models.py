1857e33c83e3cb0588d4230249d2bc412507ad14
Arxonait <nikk23716@gmail.com>
2023-10-29 08:07:29 +0300

    модель service

--- server/website/models.py
+++ server/website/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 # Create your models here.
-class Users(models):
+class Users(models.Model):
     name = models.TextField()
     phone = models.TextField(max_length=11)
     email = models.TextField()
@@ -20,3 +20,28 @@ class UserRoles(enum.Enum):
     customer = "заказчик"
     engineer = "инжинер"
     company = "уполномоченный от предприятия"
+
+
+class ServiceSectors(models.Model):
+    name_sector = models.TextField()
+    about = models.TextField()
+
+
+class Services(models.Model):
+    user = models.ForeignKey("Users", on_delete=models.CASCADE)
+    sector = models.ForeignKey("ServiceSectors", on_delete=models.CASCADE)
+
+    type_service = models.TextField()
+
+    name_service = models.TextField()
+    price = models.FloatField()
+    about = models.TextField()
+
+    name_company = models.TextField(null=True)
+
+
+class TypesService(enum.Enum):
+    engineer = "инжинер"
+    company = "предприятие"
+
+
